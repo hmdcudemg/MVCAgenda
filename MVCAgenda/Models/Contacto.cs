0b87c5773f56e30ead767e6863a88a10ba2584ef
David Mendez Guardado <demg@outlook.com>
2016-10-28 15:08:20 -0500

    .
    -MVC4, ya instalado EntityFramework y Asp.Net.Web.Optimization

---
+++ MVCAgenda/Models/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MVC4_Agenda.Models
+{
+    public class Contacto
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string ApePaterno { get; set; }
+        public string ApeMaterno { get; set; }
+        public string Puesto { get; set; }
+        public string Organizacion { get; set; }
+        public string MostrarComo { get; set; }
+        public string PaginaWeb { get; set; }
+        public string DireccionTM { get; set; }
+
+    }
+    public class ContactoDBContext : DbContext
+    {
+        public DbSet<Contacto> Contactos { set; get; }
+    }
+}
